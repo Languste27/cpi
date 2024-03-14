d64defab2a0ae45e5c75b31bc105805fa926f650
Languste <maximilian.afemann@gmail.com>
2024-03-14 21:12:55 +0100

    setting up for pip

---
+++ cpi/generator.py
@@ -0,0 +1,6 @@
+class Generator:
+    def __init__(self, mc: int, seed: int, dimension: int) -> None:
+
+        self.version = mc
+        self.seed = seed
+        self.dimension = dimension
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cpi",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # List your library's dependencies here
+    author="Languste",
+    author_email="your.email@example.com",
+    description="This library is a python interface for cubiomes",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Languste27/cpi",
+)
