3ed641dcd79961888897b06667574de4f87bcef8
Languste <maximilian.afemann@gmail.com>
2024-03-14 21:57:17 +0100

    setup for pip

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cpi",
+    name="cubiomespi",
     version="0.1",
     packages=find_packages(),
-    package_data={'your_library': ['lib/lib.dll']},
+    package_data={'cubiomespi': ['lib/lib.dll']},
     install_requires=[],  # List your library's dependencies here
     author="Languste",
-    author_email="your.email@example.com",
+    author_email="maximilian.afemann@gmail.com",
     description="This library is a python interface for cubiomes",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
