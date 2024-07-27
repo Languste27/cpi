6aa289e08b3cfae3b47c5583ad50e85467f20b46
cylorun <allidabbi@gmail.com>
2024-07-27 02:38:59 +0000

    changed tests

--- src/test.py
+++ src/test.py
@@ -10,5 +10,9 @@ g = Generator(MCVersion.MC_1_16_1, 1, Dimension.DIM_OVERWORLD)
 
 st = time.time_ns()
 l = get_stronghold_pos(g, 3)
+
 print(f'ms: {time.time_ns() - st}')
-print(l)
+if (l == [[-220, -1916], [1556, 660], [-1484, 1204]]):
+    print('success')
+    exit(0)
+exit(1)
